f92dc37e01fd67bd0972ce95e94e794bfd600230
giuseppecolucci <giuseppecolucciwork@gmail.com>
2021-01-26 14:25:29 +0100

    finito ma parto con l'esplorazione di vue ;-)

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const collegamentoHtml=  new Vue({
+    el:'#connect',
+    data:{
+        title:'Benvenuto in Vue',
+        image:'https://i.pinimg.com/originals/6e/da/85/6eda85f4d6ab544ce3308917bbbb7138.jpg'
+    }
+});// fine di vue!!!
+
+/*Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+
+Aggiungere un’immagine presa anch’essa da un data
+ */
