6450df70ef28edfaf7700b0b6a0b2b339f166b0c
giuseppecolucci <giuseppecolucciwork@gmail.com>
2021-01-26 16:18:43 +0100

    v-bind color????

--- BONUS/js/main.js
+++ BONUS/js/main.js
@@ -5,6 +5,7 @@ const collegamentoHtml=  new Vue({
         image:'https://i.pinimg.com/originals/6e/da/85/6eda85f4d6ab544ce3308917bbbb7138.jpg',
         classe:'provaClass',
         inverto:'',
+        color:'#007c92'
     },
     //funzioni
     methods:{
