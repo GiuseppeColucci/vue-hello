f65cc14dfa8ceba61002c9b7e09c6a68a04b920a
giuseppecolucci <giuseppecolucciwork@gmail.com>
2021-01-26 15:14:14 +0100

    fatto anche il bonus ma voglio andare oltre

---
+++ BONUS/js/main.js
@@ -0,0 +1,17 @@
+const collegamentoHtml=  new Vue({
+    el:'#connect',
+    data:{
+        title:'Benvenuto in Vue',
+        image:'https://i.pinimg.com/originals/6e/da/85/6eda85f4d6ab544ce3308917bbbb7138.jpg',
+        classe:'provaClass',
+        inverto:'',
+    },
+    //funzioni
+    methods:{
+        Boom: function(){
+            alert('boom!!!!')
+        }
+    }
+});// fine di vue!!!
+
+
